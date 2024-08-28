5acbfe7657155460acbe353357524b5b7e0a9c3b
CodesWithError <120249540+CodesWithError@users.noreply.github.com>
2024-08-28 22:05:48 +0530

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,9 @@
+document.getElementById("button1").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
+
+document.getElementById("button2").addEventListener("click", function () {
+    window.location.href = "sorting.html";
+});
+
+
